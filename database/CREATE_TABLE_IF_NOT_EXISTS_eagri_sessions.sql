9e0d0ebd578249152e4663f0b62b9cdc33759881
Hitesh <Hitesh.ingale@mswipe.in>
2018-11-13 11:14:51 +0530

    sessions table

---
+++ database/CREATE_TABLE_IF_NOT_EXISTS_eagri_sessions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `eagri_sessions` (
+        `id` varchar(128) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        KEY `ci_sessions_timestamp` (`timestamp`),
+        PRIMARY KEY (`id`)
+	);
